1280f35db96b78965603c4423c38b3172f24f9da
JoaquinNavio <joaconavio10@gmail.com>
2024-05-16 15:48:47 -0300

    falta crud

--- src/components/UnidadMedida/UnidadMedida.tsx
+++ src/components/UnidadMedida/UnidadMedida.tsx
@@ -13,14 +13,12 @@ import { toggleModal } from "../../redux/slices/ModalReducer";
 import { handleSearch, onDelete } from "../../utils/utils";
 
 const UnidadMedida = () => {
-    /*const url = import.meta.env.VITE_API_URL;
+    const url = import.meta.env.VITE_API_URL;
     const dispatch = useAppDispatch();
     const  unidadMedidaService = new UnidadMedidaService();
     const globalUnidadMedida = useAppSelector(
         (state) => state.unidadMedida.data
       );
-
-
 
       const [filteredData, setFilteredData] = useState<IUnidadMedida[]>([]);
 
@@ -38,9 +36,7 @@ const UnidadMedida = () => {
         }
       };
 
-      const columns: Column[] = [
-        { id: "Denominacion", label: "Denominacion", renderCell: (unidadMedida) => <>{unidadMedida.nombre}</> },
-      ];
+
 
       const handleAddUnidadMedida = () => {
         dispatch(toggleModal({ modalName: "modal" }));
@@ -67,9 +63,45 @@ const UnidadMedida = () => {
         } catch (error) {
           console.error("Error al eliminar empresa:", error);
         }
-      };*/
+      };
+
+
+      const handleEdit = (unidadMedida: IUnidadMedida) => {
+      };
+
+      const columns: Column[] = [
+        { id: "Denominacion", label: "Denominacion", renderCell: (unidadMedida) => <>{unidadMedida.denominacion}</> },
+      ];
   return (
     <div>
+    <Box component="main" sx={{ flexGrow: 1, my: 10 }}>
+      <Container>
+        <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", my: 1 }}>
+          <Typography variant="h5" gutterBottom>
+            Unidades de medida
+          </Typography>
+          <Button
+            onClick={handleAddUnidadMedida}
+            sx={{
+              bgcolor: "#ha4444",
+              "&:hover": {
+                bgcolor: "#hb6666",
+              },
+            }}
+            variant="contained"
+            startIcon={<Add />}
+          >
+            Añadir Unidad
+          </Button>
+        </Box>
+        <Box sx={{ mt: 2 }}>
+          <SearchBar onSearch={onSearch} />
+        </Box>
+        <TableComponent data={filteredData} columns={columns} onDelete={onDeleteUnidadMedida} onEdit={handleEdit} />
+      </Container>
+    </Box>
+
+
     </div>
   )
 }
--- src/types/UnidadMedida.ts
+++ src/types/UnidadMedida.ts
@@ -2,5 +2,4 @@ import DataModel from "./DataModel";
 
 export default interface IUnidadMedida extends DataModel<IUnidadMedida>{
     denominacion: string;
-
 }
