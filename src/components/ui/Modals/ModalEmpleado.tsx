ced7d5c78de1ae4985044621d95e3dd193105a61
Igna Valdivia <ignacio.valdivia@alumnos.frm.utn.edu.ar>
2024-06-17 19:59:00 -0300

    pais,localidad,provincia en modal empleado

--- src/components/ui/Modals/ModalEmpleado.tsx
+++ src/components/ui/Modals/ModalEmpleado.tsx
@@ -9,18 +9,43 @@ import { Formik, Field, FieldArray, FormikProps, Form } from 'formik';
 import { Button, Col, Row, Card } from 'react-bootstrap';
 import ILocalidad from '../../../types/ILocalidad';
 import { TextField } from '@mui/material';
-import { Add } from '@mui/icons-material';
+//import { Add } from '@mui/icons-material';
 import './modal.css';
 import IEmpleado from '../../../types/Empleado';
+import LocalidadService from '../../../services/LocalidadService';
+import ProvinciaService from '../../../services/ProvinciaService';
+import PaisService from '../../../services/PaisService';
+import { useAuth0 } from '@auth0/auth0-react';
+import IPais from '../../../types/IPais';
+import IProvincia from '../../../types/IProvincia';
+import SelectList from '../SelectList/SelectList';
 
 
 const ModalEmpleado: React.FC<{ initialValues: IEmpleado, isEditMode: boolean, onSubmit: (values: IEmpleado) => Promise<void> }> = ({ initialValues, isEditMode, onSubmit }) => {
   const dispatch = useDispatch();
   const showModal = useSelector((state: RootState) => state.modal.modalEmpleado);
-  const localidades = useSelector((state: RootState) => state.localidad.data);
+ // const localidades = useSelector((state: RootState) => state.localidad.data);
 
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
   const [imageInputs, setImageInputs] = useState<number[]>([]);
+
+
+  const { getAccessTokenSilently } = useAuth0();
+  const URL = import.meta.env.VITE_API_URL;
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        const accessToken = await getAccessTokenSilently();
+        setToken(accessToken);
+      } catch (error) {
+        console.error('Error al obtener el token:', error);
+      }
+    };
+
+    fetchToken();
+  }, [getAccessTokenSilently]);
 
   useEffect(() => {
     if (showModal) {
@@ -96,6 +121,83 @@ const ModalEmpleado: React.FC<{ initialValues: IEmpleado, isEditMode: boolean, o
 
   const addNewImageInput = () => {
     setImageInputs([...imageInputs, imageInputs.length]);
+  };
+
+  const [paises, setPaises] = useState<IPais[]>([]);
+  const [provincias, setProvincias] = useState<IProvincia[]>([]);
+  const [localidades, setLocalidades] = useState<ILocalidad[]>([]);
+
+  const [selectedPaisId, setSelectedPaisId] = useState<number | undefined>(undefined);
+  const [selectedProvinciaId, setSelectedProvinciaId] = useState<number | undefined>(undefined);
+  const [selectedLocalidadId, setSelectedLocalidadId] = useState<number | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchPaises = async () => {
+      if (token) {
+        try {
+          const paisService = new PaisService();
+          const paises = await paisService.getAll(`${URL}/pais`, token);
+          setPaises(paises);
+        } catch (error) {
+          console.error('Error al obtener los países:', error);
+        }
+      }
+    };
+
+    fetchPaises();
+  }, [token, URL]);
+
+  useEffect(() => {
+    const fetchProvincias = async () => {
+      if (token && selectedPaisId) {
+        try {
+          const provinciaService = new ProvinciaService();
+          const provincias = await provinciaService.getByPais(`${URL}/provincia/findByPais`, selectedPaisId, token);
+          setProvincias(provincias);
+        } catch (error) {
+          console.error('Error al obtener las provincias:', error);
+        }
+      }
+    };
+
+    fetchProvincias();
+  }, [selectedPaisId, token, URL]);
+
+  useEffect(() => {
+    const fetchLocalidades = async () => {
+      if (token && selectedProvinciaId) {
+        try {
+          const localidadService = new LocalidadService();
+          const localidades = await localidadService.getByProvincia(`${URL}/localidad/findByProvincia`, selectedProvinciaId, token);
+          setLocalidades(localidades);
+        } catch (error) {
+          console.error('Error al obtener las localidades:', error);
+        }
+      }
+    };
+
+    fetchLocalidades();
+  }, [selectedProvinciaId, token, URL]);
+
+  const handlePaisChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const paisIdSelected = parseInt(event.target.value);
+    setSelectedPaisId(paisIdSelected);
+    setSelectedProvinciaId(undefined);
+    setSelectedLocalidadId(undefined);
+    setProvincias([]);
+    setLocalidades([]);
+  };
+
+  const handleProvinciaChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const provinciaIdSelected = parseInt(event.target.value);
+    setSelectedProvinciaId(provinciaIdSelected);
+    setSelectedLocalidadId(undefined);
+    setLocalidades([]);
+  };
+
+  const handleLocalidadChange = (event: ChangeEvent<HTMLSelectElement>) => {
+    const localidadIdSelected = parseInt(event.target.value);
+    setSelectedLocalidadId(localidadIdSelected);
   };
 
   return (
@@ -177,13 +279,44 @@ const ModalEmpleado: React.FC<{ initialValues: IEmpleado, isEditMode: boolean, o
                             <Field name={`domicilios[${index}].nroDpto`} placeholder="Nro Dpto" className="form-control" />
                           </Col>
                           <Col>
-                            <label htmlFor={`domicilios[${index}].localidad.id`}>Localidad</label>
-                            <Field as="select" name={`domicilios[${index}].localidad.id`} className="form-control">
-                              {localidades.map((loc: ILocalidad) => (
-                                <option key={loc.id} value={loc.id}>{loc.nombre}</option>
-                              ))}
-                            </Field>
+                          <SelectList
+                          title="País"
+                          items={paises.reduce((mapa, pais) => {
+                            mapa.set(pais.id, pais.nombre);
+                            return mapa;
+                          }, new Map<number, string>())}
+                          handleChange={handlePaisChange}
+                          selectedValue={selectedPaisId}
+                        />
                           </Col>
+                        </Row>
+                        <Row>
+                          {selectedPaisId && (
+                            <div style={{ flex: 1 }}>
+                              <SelectList
+                                title="Provincia"
+                                items={provincias.reduce((mapa, provincia) => {
+                                  mapa.set(provincia.id, provincia.nombre);
+                                  return mapa;
+                                }, new Map<number, string>())}
+                                handleChange={handleProvinciaChange}
+                                selectedValue={selectedProvinciaId}
+                              />
+                            </div>
+                          )}
+                          {selectedProvinciaId && (
+                            <div style={{ flex: 1 }}>
+                              <SelectList
+                                title="Localidad"
+                                items={localidades.reduce((mapa, localidad) => {
+                                  mapa.set(localidad.id, localidad.nombre);
+                                  return mapa;
+                                }, new Map<number, string>())}
+                                handleChange={handleLocalidadChange}
+                                selectedValue={selectedLocalidadId}
+                              />
+                            </div>
+                          )}
                         </Row>
                         <Button variant="danger" type="button" onClick={() => remove(index)}>Eliminar Domicilio</Button>
                       </Card.Body>
