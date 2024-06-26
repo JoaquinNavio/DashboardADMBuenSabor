b030a961445d306d6822de55af08440499774970
JoaquinNavio <joaconavio10@gmail.com>
2024-06-26 12:51:24 -0300

    "callbackpage"

--- src/components/auth0/CallbackPage.tsx
+++ src/components/auth0/CallbackPage.tsx
@@ -1,5 +1,16 @@
-const CallbackPage = () => {
-  return <div></div>;
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+
+const Callback = () => {
+  const { isAuthenticated } = useAuth0();
+
+  React.useEffect(() => {
+    if (isAuthenticated) {
+      window.location.href = '/'; // Redirige a una página específica después de la autenticación
+    }
+  }, [isAuthenticated]);
+
+  return <div>Loading...</div>;
 };
 
-export default CallbackPage;
+export default Callback;
